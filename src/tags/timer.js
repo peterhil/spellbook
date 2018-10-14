3f431bc38e29be1ba4e9210e5ff0a3ae925b39b6
Peter Hillerström <peter.hillerstrom@gmail.com>
2018-10-15 01:24:30 +0300

    Fix tags/timer.js

--- src/tags/timer.js
+++ src/tags/timer.js
@@ -1,13 +1,11 @@
-export default function timer (opts) {
-  this.time = opts.start || 0
+this.time = opts.start || 0
 
-  tick() {
-    this.update({ time: ++this.time })
-  }
-
-  var timer = setInterval(this.tick, 1000)
-
-  this.on('unmount', function () {
-    clearInterval(timer)
-  })
+tick() {
+  this.update({ time: ++this.time })
 }
+
+var timer = setInterval(this.tick, 1000)
+
+this.on('unmount', function () {
+  clearInterval(timer)
+})
