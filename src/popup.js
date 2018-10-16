5e8de20c03e202949fcea97899cff155d3731430
Peter Hillerström <peter.hillerstrom@gmail.com>
2018-10-16 15:41:22 +0300

    Implement beforeunload event handler to unload Kefir stream subscriptions

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2018 Peter Hillerström. All rights reserved.
 
-/* global document */
+/* global document, window */
 
 import riot from 'riot'
 import Kefir from 'kefir'
@@ -8,11 +8,23 @@ import './tags/app.tag'
 
 import './popup.sass'
 
+const domStream = Kefir.fromEvents(document, 'DOMContentLoaded')
+
 function onPopup (event) {
   riot.mount('app')
   return true
 }
 
-const domStream = Kefir.fromEvents(document, 'DOMContentLoaded')
 domStream.onValue(onPopup)
   .log()
+
+// Cleanup
+
+function onUnload (event) {
+  domStream.offValue(onPopup)
+  window.removeEventListener('beforeunload', onUnload)
+}
+
+// Note! This prevents browsers from using in-memory page navigation caches,
+// see: https://developer.mozilla.org/en-US/docs/Web/Events/beforeunload
+window.addEventListener('beforeunload', onUnload)
