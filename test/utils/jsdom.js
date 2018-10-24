e67b7a2625f2b5a3058502019a3f376cd7c6fa31
Peter Hillerström <peter.hillerstrom@gmail.com>
2018-10-24 21:26:58 +0300

    Rename test/utils/dom.js as jsdom.js

--- test/utils/jsdom.js
+++ test/utils/jsdom.js
@@ -1,8 +1,13 @@
-// Using jsdom
+// Copyright (c) 2018 Peter Hillerström and contributors
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// Using jsdom:
 //
 // https://dustinpfister.github.io/2018/01/11/nodejs-jsdom/
 // https://github.com/airbnb/enzyme/issues/942#issuecomment-314715229
-
 const jsdom = require('jsdom').JSDOM
 const html = '<!doctype html><html><body></body></html>'
 const dom = new jsdom(html)
