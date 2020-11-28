6365f8ee74fcc88810c5687930dcce5332acf578
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-11-28 04:24:48 +0200

    Remove beforeunload event handler

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -4,8 +4,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-/* global window */
-
 import Popup from '../components/Popup.svelte'
 import { domLoaded$ } from '../lib/events'
 import { disconnectionHandler, messageBridge } from '../lib/messaging'
@@ -31,15 +29,4 @@ function onLoad (event) {
   })
 }
 
-function onUnload (event) {
-  domLoaded$.offValue(onLoad)
-  window.removeEventListener('beforeunload', onUnload)
-}
-
 domLoaded$.onValue(onLoad)
-
-// Cleanup
-//
-// Note! This prevents browsers from using in-memory page navigation caches,
-// see: https://developer.mozilla.org/en-US/docs/Web/Events/beforeunload
-window.addEventListener('beforeunload', onUnload)
