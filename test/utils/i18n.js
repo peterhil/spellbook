7286d69c6ade0a99c81d8cb0a6e6daf21268d1ab
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-05-30 02:41:01 +0300

    test: Cleanup test/utils/i18n.js

--- test/utils/i18n.js
+++ test/utils/i18n.js
@@ -1,18 +1,25 @@
 import fs from 'fs'
 import path from 'path'
+
 import { __dirname } from './meta.js'
 
-let rawdata = ''
-try {
-  rawdata = fs.readFileSync(path.resolve(__dirname, `../../src/_locales/${getUserLocale()}/messages.json`))
-} catch (err) {
-  rawdata = fs.readFileSync(path.resolve(__dirname, `../../src/_locales/en/messages.json`))
-}
-const messages = JSON.parse(rawdata)
-
-export function getUserLocale() {
+function getUserLocale () {
   return Intl.DateTimeFormat().resolvedOptions().locale.slice(0, 2)
 }
+
+function getLocalizedMessages (languageCode) {
+  let rawdata = ''
+
+  try {
+    rawdata = fs.readFileSync(path.resolve(__dirname, `../../src/_locales/${languageCode}/messages.json`))
+  } catch (err) {
+    rawdata = fs.readFileSync(path.resolve(__dirname, '../../src/_locales/en/messages.json'))
+  }
+
+  return JSON.parse(rawdata)
+}
+
+const messages = getLocalizedMessages(getUserLocale())
 
 export function t(key) {
   return messages[key].message
