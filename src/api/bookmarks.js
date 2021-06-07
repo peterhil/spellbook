e4d7c56c34b7c9af1a4a23b8701954149f12d805
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-06-07 05:43:47 +0300

    lint: Remove unused Kefir imports

--- src/api/bookmarks.js
+++ src/api/bookmarks.js
@@ -4,7 +4,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import { Kefir } from 'kefir'
 import { choice, sortByTitleCaseInsensitive } from '../lib/pure'
 import { notImplemented$ } from '../lib/reactive'
 import * as chromeBookmarks from './chrome/bookmarks'
--- src/background/popup-controller.js
+++ src/background/popup-controller.js
@@ -4,8 +4,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import Kefir from 'kefir'
-
 import { sendMessage, unhandledMessage } from '../lib/messaging'
 import { choice } from '../lib/pure'
 import { categorySearch, searchWithBookmark } from '../api/bookmarks'
