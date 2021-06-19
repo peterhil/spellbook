7f7f7cc7698eabbe664c9115048bb8a2ed3f8094
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-06-19 22:40:42 +0300

    i18n: Localise explore and organise tabs #44

--- src/js/components/Explore.svelte
+++ src/js/components/Explore.svelte
@@ -1,10 +1,14 @@
+<script>
+    import { t } from '../lib/translate'
+</script>
+
 <div class="card">
     <ul class="tab tab-block">
         <li class="tab-item active">
-            <a href="#">Explore</a>
+            <a href="#">{ t('tab_explore') }</a>
         </li>
         <li class="tab-item">
-            <a href="#">Organise</a>
+            <a href="#">{ t('tab_organise') }</a>
         </li>
     </ul>
 </div>
