980ec24e2d6dd84876cf8ea0bf763680c3166760
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-06-21 05:34:58 +0300

    Move styles from DirectoryHeader to directory.scss #54

--- src/js/components/DirectoryHeader.svelte
+++ src/js/components/DirectoryHeader.svelte
@@ -2,32 +2,6 @@
     import { t } from '../lib/translate'
 </script>
 
-<style type="scss">
-    .navbar {
-        padding: 0 0.8rem;
-        color: var(--primary-text);
-        background: no-repeat 0 38% / cover url('/img/spellbook-bg.jpg') var(--primary-color);
-        border: 0;
-    }
-
-    .navbar > * {
-        line-height: 30px;
-    }
-
-    h1 {
-        margin: 0;
-        padding: 0;
-        font-size: 20px;
-    }
-
-    .search {
-        padding: 0 0.4rem;
-        font-size: 14px;
-        line-height: 28px;
-        border: 1px solid #555;
-    }
-</style>
-
 <header class="navbar">
     <section class="navbar-section">
         <h1>Spellbook</h1>
