99db49f020d77350d5cc747f6c21c28d047f709f
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-06-27 20:30:20 +0300

    Use $search.last on status #56

--- src/js/components/BookmarkForm.svelte
+++ src/js/components/BookmarkForm.svelte
@@ -82,7 +82,7 @@
                 {#if isVisible('search') && $search.last }
                     <span class="label" title="{ t('search') }">
                         <IconFa icon="search" />
-                        { $search.query }
+                        { $search.last }
                     </span>
                 {/if}
             </span>
