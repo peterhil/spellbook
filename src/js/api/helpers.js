663d07de2c86e500c69eea425851e8ea7da7d8d3
Peter Hillerström <peter.hillerstrom@gmail.com>
2022-02-16 14:00:09 +0200

    Remove unused import of zepto-detect

--- src/js/api/helpers.js
+++ src/js/api/helpers.js
@@ -6,7 +6,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import zd from 'zepto-detect'
 import { prop } from 'rambda'
 
 import { fromEventPattern } from '../lib/rxjs'
