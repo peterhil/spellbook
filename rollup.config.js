1a30c79b39b1572acb76512fff9a2a5c15676dfc
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-07-06 18:28:56 +0300

    rollup: Bring back Rollup plugin configs
    This reverts commit 9f477260118e8256d3b3f411d14fbbd883ff6aaf.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,94 @@
+import commonjs from '@rollup/plugin-commonjs'
+import copy from 'rollup-plugin-copy'
+import eslint from '@rollup/plugin-eslint'
+import resolve from '@rollup/plugin-node-resolve'
+import preprocess from 'svelte-preprocess'
+import sass from 'rollup-plugin-sass'
+import svelte from 'rollup-plugin-svelte'
+import { terser } from '@rollup/plugin-terser'
+
 import { isDev, outputDir, rel, urlPath } from './utils.config'
 
 const format = 'es'
 const sourcemap = (isDev ? 'inline' : false)
 const verbose = true
+
+const plugins = [
+    eslint({
+        exclude: [
+            'src/**/*.{css,sass}',
+        ]
+    }),
+
+    svelte({
+        emitCss: true,
+        compilerOptions: {
+            // enable run-time checks when not in production
+            dev: isDev,
+        },
+        preprocess: preprocess({
+            style: sass(),
+        }),
+    }),
+
+    sass(),
+
+    // Convert CommonJS libraries to ES6
+    resolve({
+        browser: true, // default: false
+        modulesOnly: false, // default: false
+        dedupe: ['svelte'],
+        modulePaths: [
+            './node_modules/'
+        ],
+        preferBuiltins: false,
+    }),
+    commonjs(),
+]
+
+const copyAssets = [
+    copy({
+        targets: [{
+            src: [
+                'src/**/*.html',
+            ],
+            dest: outputDir('views'),
+        }],
+        flatten: true,
+        verbose,
+    }),
+    copy({
+        targets: [{
+            src: [
+                'src/_locales/**/*.json',
+                'src/img/spellbook-bg.jpg',
+                'src/img/spellbook_icon*.png',
+                // 'src/manifest.json',
+            ],
+            dest: outputDir(),
+        }],
+        flatten: false,
+        verbose,
+    }),
+    // copy({
+    //     targets: [{
+    //         src: [
+    //             'node_modules/spectre.css/dist/spectre-icons.css',
+    //             'node_modules/spectre.css/dist/spectre.css',
+    //         ],
+    //         dest: outputDir('style'),
+    //     }],
+    //     flatten: true,
+    //     verbose,
+    // }),
+]
+
+const watch = {
+    chokidar: true,
+    clearScreen: true,
+    exclude: ['node_modules/**'],
+    include: ['src/**/*'],
+}
 
 export default {
     input: {
@@ -27,4 +113,6 @@ export default {
         },
         sourcemap,
     },
+    plugins: plugins.concat(copyAssets),
+    watch,
 }
