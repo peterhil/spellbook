c0abe1ff87fcd9324560e6227e602b379c45b46f
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-07-30 00:06:09 +0300

    Log a warning on unsupported URLs on searchWithUrl #90

--- src/js/api/bookmarks.js
+++ src/js/api/bookmarks.js
@@ -4,8 +4,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import zd from 'zepto-detect'
-import { test } from 'rambda'
 import browser from 'webextension-polyfill'
 
 import { sortByTitleCaseInsensitive } from '../lib/pure'
@@ -22,21 +20,18 @@ export const categorySearch = async (query) => {
     })
 }
 
-export function searchWithUrl (url) {
+export async function searchWithUrl (url) {
+    let bookmarks = []
+
     // console.debug('searchWithUrl:', url)
     if (!url) return []
 
-    if (zd.browser.firefox && test(/^about:/, url)) {
-        // Query object with 'about:' url scheme will throw a
-        // SecurityError and a TypeError, so query with string even if
-        // it might return partial matches.
-
-        // A string query on Chrome replaces special characters with spaces,
-        // so for example 'chrome://extensions' would return all bookmarks
-        // with chrome and extensions...
-
-        return browser.bookmarks.search(url)
+    try {
+        bookmarks = await browser.bookmarks.search({ url })
+    }
+    catch (err) {
+        console.warn('[api/bookmarks] Unsupported URL:', url)
     }
 
-    return browser.bookmarks.search({ url })
+    return bookmarks
 }
