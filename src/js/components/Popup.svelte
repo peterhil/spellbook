71b97fa5ba5eedb2b46036b20fd510a6bbd7d361
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-02-09 12:02:40 +0200

    Move currentTabStatus from Popup into TabAdd #44

--- src/js/components/Popup.svelte
+++ src/js/components/Popup.svelte
@@ -1,39 +1,15 @@
 <script>
-    import browser from 'webextension-polyfill'
-
-    import { onMount } from 'svelte'
-
     import TabAdd from './TabAdd.svelte'
     import TabUrlSearch from './TabUrlSearch.svelte'
     import Tabs from './Tabs.svelte'
 
-    import { bookmarkCountChanged$ } from '../api/streams'
-    import { activeTabQuery } from '../api/tabs'
     import { t } from '../lib/translate'
-    import { currentTab } from '../stores/currentTab'
 
     const tabs = [
         { id: 'add', label: t('tab_add'), component: TabAdd },
         { id: 'url', label: t('tab_url'), component: TabUrlSearch },
     ]
     let mode = tabs[0]
-
-    async function currentTabStatus () {
-        const tabs = await browser.tabs.query(activeTabQuery)
-        const tab = tabs[0]
-
-        // console.debug('[Popup] currentTabStatus:', tab)
-        if (tab) {
-            $currentTab = { ...tab }
-        }
-    }
-
-    onMount(() => {
-        currentTabStatus()
-
-        // Refresh contents when bookmarks change
-        bookmarkCountChanged$.observe(currentTabStatus)
-    })
 </script>
 
 <div class="popup">
--- src/js/components/TabAdd.svelte
+++ src/js/components/TabAdd.svelte
@@ -1,7 +1,30 @@
 <script>
+    import browser from 'webextension-polyfill'
+
+    import { onMount } from 'svelte'
+
     import BookmarkForm from './BookmarkForm.svelte'
 
+    import { bookmarkCountChanged$ } from '../api/streams'
+    import { activeTabQuery } from '../api/tabs'
     import { currentTab } from '../stores/currentTab'
+
+    async function currentTabStatus () {
+        const tabs = await browser.tabs.query(activeTabQuery)
+        const tab = tabs[0]
+
+        // console.debug('[Popup] currentTabStatus:', tab)
+        if (tab) {
+            $currentTab = { ...tab }
+        }
+    }
+
+    onMount(() => {
+        currentTabStatus()
+
+        // Refresh contents when bookmarks change
+        bookmarkCountChanged$.observe(currentTabStatus)
+    })
 </script>
 
 <div class="card">
