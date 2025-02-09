ae4135d083dab3c400a17e89f5b303f7e406758c
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-02-09 19:38:03 +0200

    Add status indicator for the number of saved bookmarks #95

--- src/js/components/Popup.svelte
+++ src/js/components/Popup.svelte
@@ -4,18 +4,30 @@
     import Tabs from './Tabs.svelte'
 
     import { t } from '../lib/translate'
+    import { savedBookmarks } from '../stores/savedBookmarks'
 
     const tabs = [
         { id: 'add', label: t('tab_add'), component: TabAdd },
         { id: 'url', label: t('tab_url'), component: TabUrlSearch },
     ]
     let mode = tabs[0]
+
+    $: bookmarkCount = $savedBookmarks.size
 </script>
 
 <div class="popup">
     <div class="popup-header">
         <div class="stripe stripe-bg">
-            <h1>Spellbook</h1>
+            <div>
+                <h1>Spellbook</h1>
+            </div>
+            <div class="status">
+                {#if bookmarkCount > 0}
+                    <h1>
+                        {bookmarkCount}
+                    </h1>
+                {/if}
+            </div>
         </div>
         <Tabs {tabs} bind:active={mode} />
     </div>
