29600643a45069b814d19397a6a2c720c870ae1f
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-02-14 22:32:17 +0200

    Move updating saved bookmarks back from TabUrlSearch into Popup component #95

--- src/js/components/Popup.svelte
+++ src/js/components/Popup.svelte
@@ -1,8 +1,16 @@
 <script>
+    import browser from 'webextension-polyfill'
+
+    import { indexBy, prop, sortBy, toPairs } from 'rambda'
+    import { onDestroy, onMount } from 'svelte'
+
     import TabAdd from './TabAdd.svelte'
     import TabUrlSearch from './TabUrlSearch.svelte'
     import Tabs from './Tabs.svelte'
 
+    import { bookmarkCountChanged$ } from '../api/streams'
+    import { getCurrentTab } from '../api/tabs'
+    import { messages } from '../lib/messaging'
     import { t } from '../lib/translate'
     import { savedBookmarks } from '../stores/savedBookmarks'
 
@@ -13,6 +21,42 @@
     let mode = tabs[0]
 
     $: bookmarkCount = $savedBookmarks.size
+
+    async function deleteBookmark (bookmark) {
+        // console.debug('Deleting bookmark:', bookmark)
+        return browser.bookmarks.remove(bookmark.id)
+            .catch(console.error)
+    }
+
+    async function getSavedBookmarks () {
+        const tab = await getCurrentTab()
+
+        // console.debug('[Popup] getSavedBookmarks:', tab)
+        messages.emit('api', { action: 'savedBookmarks', tab })
+    }
+
+    function updateSavedBookmarks (bookmarks) {
+        const sorted = sortBy(prop('dateAdded'), bookmarks || [])
+        const saved = new Map(toPairs(indexBy(prop('id'), sorted)))
+        // console.debug('[Popup] updateBookmarks sorted:', { bookmarks, sorted, saved })
+
+        $savedBookmarks = saved
+    }
+
+    onMount(() => {
+        messages.on('deleteBookmark', deleteBookmark)
+        messages.on('savedBookmarks', updateSavedBookmarks)
+
+        getSavedBookmarks()
+
+        // Refresh contents when bookmarks change
+        bookmarkCountChanged$.observe(getSavedBookmarks)
+    })
+
+    onDestroy(() => {
+        messages.off('deleteBookmark', deleteBookmark)
+        messages.off('savedBookmarks', updateSavedBookmarks)
+    })
 </script>
 
 <div class="popup">
--- src/js/components/TabUrlSearch.svelte
+++ src/js/components/TabUrlSearch.svelte
@@ -1,14 +1,6 @@
 <script>
-    import browser from 'webextension-polyfill'
-
-    import { indexBy, prop, sortBy, toPairs } from 'rambda'
-    import { onDestroy, onMount } from 'svelte'
-
     import Bookmark from './Bookmark.svelte'
 
-    import { bookmarkCountChanged$ } from '../api/streams'
-    import { getCurrentTab } from '../api/tabs'
-    import { messages } from '../lib/messaging'
     import { t } from '../lib/translate'
     import { savedBookmarks } from '../stores/savedBookmarks'
 
@@ -18,42 +10,6 @@
             ? t('saved_bookmark')
             : t('add_bookmark')
     )
-
-    async function deleteBookmark (bookmark) {
-        // console.debug('Deleting bookmark:', bookmark)
-        return browser.bookmarks.remove(bookmark.id)
-            .catch(console.error)
-    }
-
-    async function getSavedBookmarks () {
-        const tab = await getCurrentTab()
-
-        // console.debug('[Popup] getSavedBookmarks:', tab)
-        messages.emit('api', { action: 'savedBookmarks', tab })
-    }
-
-    function updateSavedBookmarks (bookmarks) {
-        const sorted = sortBy(prop('dateAdded'), bookmarks || [])
-        const saved = new Map(toPairs(indexBy(prop('id'), sorted)))
-        // console.debug('[Popup] updateBookmarks sorted:', { bookmarks, sorted, saved })
-
-        $savedBookmarks = saved
-    }
-
-    onMount(() => {
-        messages.on('deleteBookmark', deleteBookmark)
-        messages.on('savedBookmarks', updateSavedBookmarks)
-
-        getSavedBookmarks()
-
-        // Refresh contents when bookmarks change
-        bookmarkCountChanged$.observe(getSavedBookmarks)
-    })
-
-    onDestroy(() => {
-        messages.off('deleteBookmark', deleteBookmark)
-        messages.off('savedBookmarks', updateSavedBookmarks)
-    })
 </script>
 
 <div class="card">
