df577d8b2447c549e7c4ce20ebc3ec25f971b4f7
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-02-15 16:16:51 +0200

    Remove unused import of browser

--- src/js/background/status.js
+++ src/js/background/status.js
@@ -4,8 +4,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import browser from 'webextension-polyfill'
-
 import { getCurrentTab } from '../api/tabs'
 import { browserAction } from '../lib/compat'
 import { searchWithUrl } from '../api/bookmarks'
