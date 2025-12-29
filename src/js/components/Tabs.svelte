70c6ba4dea3b824df2d43f3e3ea60d4ea333e3d7
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-12-29 18:28:05 +0200

    Fix legacy event handler helper on Tabs component

--- src/js/components/Tabs.svelte
+++ src/js/components/Tabs.svelte
@@ -1,7 +1,5 @@
 <script>
-    import { preventDefault } from 'svelte/legacy';
-
-    let { tabs, active = $bindable({}) } = $props();
+    let { tabs, active = $bindable({}) } = $props()
 
     function activate (tab) {
         active = tab
@@ -12,7 +10,7 @@
     {#each tabs as tab}
         <li class="tab-item" class:active={ active.id === tab.id }>
             <a href="#{ tab.id }"
-               onclick={preventDefault(() => activate(tab))}
+               onclick={(event) => {event.preventDefault(); activate(tab)}}
                >
                 { tab.label }
             </a>
