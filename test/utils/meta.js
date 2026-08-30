10cb33aa5ef3df313ff1ac8e88c5e552a71f0a3a
Peter Hillerström <peter.hillerstrom@gmail.com>
2026-08-30 22:16:38 +0300

    Remove nonfunctional tests

--- test/utils/meta.js
+++
@@ -1,7 +0,0 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-
-const metaUrl = import.meta.url
-
-export const __filename = fileURLToPath(metaUrl)
-export const __dirname = dirname(__filename)
